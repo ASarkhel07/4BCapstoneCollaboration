738799e1f659cff5c326ea8ca6b35dd816acf424
AlvinVarg <alvinmvarghese@gmail.com>
2024-10-28 14:07:11 -0400

    FirstTaskSmindy
    i coded smindy's first task for smindys repository

--- CollabProgram.java
+++ CollabProgram.java
@@ -2,7 +2,20 @@ public class CollabProgram
 {
 	public static void main(String[]args)
 	{
+		createSquareArray(5, 5);
+	}
 
+
+	public static void createSquareArray(int sizeX, int sizeY)
+	{
+		int[][] arr = new int[sizeX][sizeY];
+		for(int i = 0; i < sizeX; i++){
+			for(int j = 0; j < sizeY; j++){
+				arr[i][j] = (int)(Math.random()*10+1);
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 
 
